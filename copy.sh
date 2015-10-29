80c4da587cc8f2e5b7f4c48a17a3b60a43ccca8a
Cassio Kirch <cassiokirch@gmail.com>
2015-10-29 11:33:15 -0200

    Argument inserted.

--- copy.sh
+++ copy.sh
@@ -5,5 +5,5 @@ read -s SSHPASS
 
 for computer in labcel1 labcel2 labcel3 labcel6 ada
 do
-    sshpass -p $SSHPASS rsync -azulvI boids4 $computer:boids4/
+    sshpass -p $SSHPASS rsync -azulvI $1 $computer:boids4/
 done
