1a0332e6bf7878749424b9d58e5b7ebab56d6c66
Cassio Kirch <cassiokirch@gmail.com>
2015-10-30 23:28:08 -0200

    Literals types.

--- define.h
+++ define.h
@@ -3,34 +3,35 @@
 #define Z        2
 #define PI       3.14159265359
 
-//#define PLOT_EXIT_FILES
-#define GAMMA_FILE
+#define PLOT_EXIT_FILES
+#define ENDO_GAMMA
+//#define ECTO_GAMMA
 
 #define ENDOPROP               0.25
-#define N                      3200
-#define RANGE                  240.0
+#define N                      800u
+#define RANGE                  25.0
 #define PROP                   0.35
-#define SEED                   13049        /* Must be unsigned int. */
-#define DIMENSIONS             2
-#define BOXES                  32400
-#define BOXES_IN_EDGE          180
+#define SEED                   13049u        /* Must be unsigned int. */
+#define DIMENSIONS             2u
+#define BOXES                  324u
+#define BOXES_IN_EDGE          18u
 #define NEIGHBOR_DISTANCE      1.32              /* r0 */
 #define CORE_RADIUS            0.4               /* rc */
 #define ELASTIC_DISTANCE       1.0               /* re */
 #define INFINITE_FORCE         10000.0
 #define ETA                    1.0
-#define V0                     0.007
-#define ALPHA11                0.01
-#define ALPHA12                0.01
-#define ALPHA22                0.01
+#define V0                     0.01
+#define ALPHA11                0.1
+#define ALPHA12                0.1
+#define ALPHA22                0.1
 #define BETA11                 0.3
 #define BETA12                 0.16
 #define BETA22                 0.15
 #define DT                     1.0
-#define DATE_SIZE              36           /* Size of string */
-#define FILENAME_SIZE          128           /* Size of string */
-#define STEPS                  250000000
-#define EXIT_INTERVAL          1000
+#define DATE_SIZE              36u           /* Size of string */
+#define FILENAME_SIZE          128u           /* Size of string */
+#define STEPS                  2500ull
+#define EXIT_INTERVAL          10
 #define NUM_THREADS            8
 
 #define RANDOM_0(max) (((double)rand()/RAND_MAX) * max)
