84c91b052cc014a07a02ca39c48d32e3044a8b0a
Cassio Kirch <cassiokirch@gmail.com>
2015-11-16 19:42:43 -0200

    New computers.

--- copy.sh
+++ copy.sh
@@ -3,7 +3,7 @@
 echo "Qual o ingrediente secreto?"
 read -s SSHPASS
 
-for computer in labcel1 labcel2 labcel3 labcel6 ada
+for computer in labcel1 labcel2 labcel3 labcel4 labcel5 labcel6 ada
 do
     sshpass -p $SSHPASS rsync -azulvI $1 $computer:boids4/
 done
